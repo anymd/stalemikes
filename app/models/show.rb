36a9a890eae8b42dc24bb985e2d54e8fe082c81e
anymd <anymd-dev@yahoo.com>
2013-09-01 19:26:59 -0700

    strip html tags on show attributes before save

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,4 +1,8 @@
+
+
 class Show < ActiveRecord::Base
+  include ActionView::Helpers::SanitizeHelper
+  before_save :remove_html
 
   attr_accessible :name, :venue, :address, :city, :state, :zip, :country, :web_site, :facebook_link, :twitter_name, :phone, :day, :frequency, :show_type, :signup_time, :start_time, :end_time, :host_name, :price, :notes, :user_id, :status, :metro_area_id, :updated_at, :charge, :filepicker_url, :verified_at, :status, :start_date, :area
 
@@ -14,6 +18,14 @@ class Show < ActiveRecord::Base
   validates :name, :venue, :address, :city, :state, :zip, :country, :day, :frequency, :show_type, :start_time, :metro_area_id, :verified_at, :status, :metro_area, :presence => true  
 
   def include_end_time
+  end
+
+  protected
+
+  def remove_html
+    self.attributes.each do |key,value|
+      self[key] = strip_tags(value) if value.class == String
+    end
   end
 
 end
