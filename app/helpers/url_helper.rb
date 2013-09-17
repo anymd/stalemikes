8c8cc0dd4113bbe4b1726ec6d50317e853d24af7
anymd <anymd-dev@yahoo.com>
2013-09-17 02:39:58 -0700

    fixing twitter link

--- app/helpers/url_helper.rb
+++ app/helpers/url_helper.rb
@@ -1,5 +1,11 @@
 module UrlHelper
+
   def url_with_protocol(url)
     /^http/.match(url) ? url : "http://#{url}"
   end
+
+  def twitter_url_with_protocol(twitter_name)
+    /^http/.match(twitter_name) ? twitter_name : "http://twitter.com/#{twitter_name}"
+  end
+
 end
